33a35b12af04786ba8c2f26a2647caa7009bd4b9
tahabornoosh <77724339+tahabornoosh@users.noreply.github.com>
2021-04-19 18:34:47 +0430

    Add time recorder

--- counters.py
+++ counters.py
@@ -1,8 +1,10 @@
 import math
+import time
 n1 = int(input('number: '))
 n2 = math.sqrt(n1)
 n3 = 1
 n4 = []
+time2 = time.time()
 while n3 != round(n2)+1:
     if(n1 % n3 == 0):
         n4.append(n3)
@@ -10,3 +12,4 @@ while n3 != round(n2)+1:
             n4.append(int(n1/n3))
     n3 +=1
 print(sorted(n4))
+print('time = '+str(time.time() - time2)+' Secondes')
